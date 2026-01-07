d2787832d3eb7deec6172435eb2bb9acb5ae1c6f
aliakbarsobhanpoor <aliakbarsobhanpoor@gmail.com>
2026-01-07 22:18:48 +0330

    add not found page.

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,10 @@
 function NotFound() {
-    return <div>not found</div>
+    return (
+        <div>
+            <h1>404 Not Found</h1>
+            <p>The page that you're looking for doesn't exist!</p>
+        </div>
+    )
 }
 
 export default NotFound
