17066991eec71f66ba9f1c8f18ae4c70363016e4
aliakbarsobhanpoor <aliakbarsobhanpoor@gmail.com>
2026-01-07 20:52:29 +0330

    add the project main component and routing code.

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import {Navigate} from "react-router-dom"
-import jwtDecode from "jwt-decode"
+import {jwtDecode} from "jwt-decode"
 import api from "../api"
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
 import { useState, useEffect } from "react"
@@ -48,6 +48,7 @@ function ProtectedRoute({chieldren}) {
     if (isAuthorized === null){
         return <div>Loading...</div>
     }
+    console.log("we are here")
     return isAuthorized ? chieldren: <Navigate to="/login"/>  
 }
 
