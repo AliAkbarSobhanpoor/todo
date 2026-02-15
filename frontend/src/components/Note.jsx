5b122d30a9ad5574c5ebdce8c96149c7840ee24f
aliakbarsobhanpoor <aliakbarsobhanpoor@gmail.com>
2026-02-15 15:21:08 +0330

    convert date to desire format.

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 function Note({ note, deleteNote }) {
-  const formatedDate = new Date(note.created_at).toLocaleDateString("en-US");
+  
+  const date = new Date(note.create_at);
 
   return (
     <>
       <div className="note-card">
         <h3 className="note-title">{note.title}</h3>
         <p className="note-content">{note.content}</p>
-        <p className="note-date">{formatedDate}</p>
+        <p className="note-date">{date.toLocaleDateString()} {date.toLocaleTimeString()}</p>
         <button className="delete-button" onClick={() => deleteNote(note.id)}>
           Delete
         </button>
